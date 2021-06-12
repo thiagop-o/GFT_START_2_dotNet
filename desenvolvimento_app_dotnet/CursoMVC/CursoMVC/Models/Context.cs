34c92dd44e15d99b0a0b16adb1b711900e1ba47b
Thiago Oliveira <thiagop_o@hotmail.com>
2021-06-12 03:56:05 -0300

    Adicionado a Aula de ASP Net MVC

---
+++ desenvolvimento_app_dotnet/CursoMVC/CursoMVC/Models/Context.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CursoMVC.Models;
+
+namespace CursoMVC.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Categoria> Produtos { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString:@"Server = localhost\SQLEXPRESS; Database = Cursomvc; Trusted_Connection = True");
+        }
+
+
+        public DbSet<CursoMVC.Models.Produto> Produto { get; set; }
+    }
+}
