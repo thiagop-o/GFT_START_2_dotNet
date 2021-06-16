ff9e13f91b131bbf9b3fba40be4cf3427a0acf61
Thiago Oliveira <thiagop_o@hotmail.com>
2021-06-16 00:17:22 -0300

    Adicionado Projeto Final do Bootcamp

---
+++ ProjetoFinal/ApiCatalogoJogos/ApiCatalogoJogos/InputModel/JogoInputModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoJogos.InputModel
+{
+    public class JogoInputModel
+    {
+        [Required]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "O nome do jogo deve conter entre 3 e 100 caracteres")]
+        public string Nome { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 1, ErrorMessage = "O nome da produtora deve conter entre 1 e 100 caracteres")]
+        public string Produtora { get; set; }
+
+        [Required]
+        [Range(1, 1000, ErrorMessage = "O preço do jogo deve no minimo R$1,00 e R$1000,00 no máximo")]
+        public double Preco { get; set; }
+
+    }
+}
